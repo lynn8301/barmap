1b50d475011ab856fed002afd090fc471be9fe28
Lynn <lynn@blueplanet.com.tw>
2020-10-27 15:09:18 +0800

    Add bar-csv-sample-file route

--- web/routes/bar.js
+++ web/routes/bar.js
@@ -1,19 +1,23 @@
 const express = require('express')
 const router = express.Router()
+const path = require('path')
 
-router.get('/', (req, res) => {
+router.get('/add', (req, res) => {
   let data = {
     title: 'BarMap',
     description: 'To explore the amazing bar',
   }
-  res.render('bar/addBar', data)
+  res.render('bar/add', data)
 })
 
-// router.get('/add', (req, res) => {
-//     let data = {
-//       title: 'BarMap',
-//     }
-//     res.render('/bar/addBar', data)
-//   })
+router.get('/bar-csv-sample-file', (req, res) => {
+  let dir = path.join(`${__dirname}`, '..', 'views/bar/file')
+  let file = `${dir}/BarSample.csv`
+  res.download(file, 'BarSample.csv', (error) => {
+    if (error) {
+      console.log(error)
+    }
+  })
+})
 
 module.exports = router
