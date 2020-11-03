33ccf167a99cb3baccc71f33dbbba138e0c54cb2
Lynn <lynn@blueplanet.com.tw>
2020-11-03 17:41:31 +0800

    Insert one data into db #1

--- web/routes/bar.js
+++ web/routes/bar.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const path = require('path')
+const base = require(`${__dirname}/../../lib/base.js`)
 
 router.get('/add', (req, res) => {
   let data = {
@@ -10,6 +11,7 @@ router.get('/add', (req, res) => {
   res.render('bar/add', data)
 })
 
+// Bar CSV Sample
 router.get('/bar-csv-sample-file', (req, res) => {
   let dir = path.join(`${__dirname}`, '..', 'views/bar/file')
   let file = `${dir}/BarSample.csv`
@@ -20,18 +22,30 @@ router.get('/bar-csv-sample-file', (req, res) => {
   })
 })
 
-router.post('/check', (req, res) => {
+// 單筆資料
+router.post('/submit', async (req, res) => {
+  let db = base.mysqlPool(base.config().development)
   let params = req.body
-  let data = {
-    title: 'BarMap',
-    description: 'To explore the amazing bar',
+  let barInfo = {
     name: params.name,
     address: params.address,
     phone: params.phone,
     lat: params.lat,
     lng: params.lng,
   }
-  res.render('bar/check', data)
+
+  await db.queryAsync('INSERT INTO bar SET ?', barInfo)
+  await db.end()
+
+  let message = `You sent the data as the following\n
+  Name: ${barInfo.name}\n
+  Address: ${barInfo.address}\n
+  Phone: ${barInfo.phone}\n
+  Lat: ${barInfo.lat}\n
+  Lng: ${barInfo.lng}`
+  
+  res.write(message)
+  res.end()
 })
 
 router.get('/map', (req, res) => {
