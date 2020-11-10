5a3a10d66ff92246d92e0f19873cf8ff66c72a80
Lynn <lynn@blueplanet.com.tw>
2020-11-10 11:57:07 +0800

    Create insert bars post #1

--- web/routes/bar.js
+++ web/routes/bar.js
@@ -2,8 +2,29 @@ const express = require('express')
 const router = express.Router()
 const path = require('path')
 const base = require(`${__dirname}/../../lib/base.js`)
-const multer  = require('multer')
-const upload = multer({dest: 'uploads/'})
+const multer = require('multer')
+const moment = require('moment')
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    let des = `tmp/uploads/${file.fieldname}`
+    base.checkPathAndMake(des)
+    cb(null, des)
+  },
+  filename: function (req, file, cb) {
+    let date = moment().format('X')
+    let typeArray = file.originalname.split('.')
+    let fileType = typeArray[typeArray.length - 1]
+
+    cb(null, `${file.fieldname}-${date}.${fileType}`)
+  },
+})
+const fileFilter = function (req, file, cb) {
+  if (!file.originalname.match(/\.(csv)$/)) {
+    return cb(new Error('Please upload CSV'), false)
+  }
+
+  cb(null, true)
+}
 
 router.get('/add', (req, res) => {
   let data = {
@@ -35,8 +56,11 @@ router.post('/submit', async (req, res) => {
     lat: params.lat,
     lng: params.lng,
   }
-  let existed = await db.queryAsync("SELECT * FROM bar WHERE name = ?", barInfo.name)
-  if(existed == 0) {
+  let existed = await db.queryAsync(
+    'SELECT * FROM bar WHERE name = ?',
+    barInfo.name,
+  )
+  if (existed == 0) {
     await db.queryAsync('INSERT INTO bar SET ?', barInfo)
   } else {
     barInfo.error = '資料以存在'
@@ -53,9 +77,36 @@ router.post('/submit', async (req, res) => {
 })
 
 // 多筆資料
-router.post('/submitCSV', upload.single('BarMultiple'), async(req, res) => {
-  let file = req.file
-  console.log(file)
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+}).single('csv-file')
+
+router.post('/submitCSV', async (req, res) => {
+  upload(req, res, async (err) => {
+    let barInfo = {}
+    if (err) {
+      barInfo.error = '請上傳正確的檔案格式'
+    } else {
+      barInfo.success = '上傳成功'
+    }
+
+    let desPath = path.join(`${__dirname}`, '..', 'tmp/uploads', 'csv-file')
+    try {
+      let info = await base.insertBars(desPath)
+      barInfo.existed = info
+    } catch (e) {
+      barInfo.error = '請再次檢查資料格式'
+    }
+    
+    let data = {
+      title: 'BarMap',
+      description: 'To explore the amazing bar',
+      barInfo: barInfo,
+    }
+    console.log(data)
+    res.render('bar/add', data)
+  })
 })
 
 router.get('/map', (req, res) => {
